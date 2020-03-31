95d7005ef4109f5b210249522e653b2649b95be9
vel murugan <velu020498@gmail.com>
2020-03-31 21:06:42 +0530

    identify customer

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 const querystring = require('querystring');
 const https = require('https');
 
+var valid_user= false //boolean value to check the user is valid or not (default value false) 
+/**
+ * Function to find the customer identity
+ * @param {string} host 
+ * @param {string} type 
+ * @param {string} value 
+ */
+function getidentity(host,type,value) {
+    https.get(`https://${host}/api/rest/acs/identity/${type}.${value}`, (res) => {
+        console.log('statusCode:', res.statusCode); // to see the status code
+        console.log('headers:', res.headers); // to see the headers for verifications
+
+        let body = "";
+
+        res.on("data", (chunk) => {
+            body += chunk;
+        });
+
+        res.on("end", () => {
+            try {
+                let json = JSON.parse(body);
+                if(json){ //condition to check the valid json 
+                    valid_user = true //change the user to valid user
+                }
+            }
+            catch (error) {
+                console.error(error.message);
+            };
+        })
+
+    }).on('error', (e) => {
+        console.error(e);
+    });
+}
+
+
 /**
  * These function will Retrieve a data from the web 
  * getting the object from the web
@@ -73,3 +109,14 @@ function updatedata(url, updated_data) {
 var updated_data = querystring.stringify({  //sample data
     'msg': 'Hello World!'
 });
+
+
+/**
+ * check the user is valid or not 
+ * if the user is valid 
+ * call  the function to create ,update or Retrieve 
+ */
+if (valid_user){
+    Retrievedata()
+    updatedata()
+}
