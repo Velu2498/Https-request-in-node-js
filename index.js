291a4b96950a12f70e6f42546211823a6eb40b58
vel murugan <velu020498@gmail.com>
2020-03-31 15:32:47 +0530

    added post method

--- index.js
+++ index.js
@@ -1,51 +1,75 @@
+const querystring = require('querystring');
 const https = require('https');
-var libxmljs = require("libxmljs");
 
-https.get('https://ghibliapi.herokuapp.com/films', (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
-  
-    let body = "";
+/**
+ * These function will Retrieve a data from the web 
+ * getting the object from the web
+ * @param {string} url - 'https://ghibliapi.herokuapp.com/films'
+ */
 
-    res.on("data", (chunk) => {
-        body += chunk;
+function Retrievedata(url) {
+
+    https.get(url, (res) => {
+        console.log('statusCode:', res.statusCode); // to see the status code
+        console.log('headers:', res.headers); // to see the headers for verifications
+
+        let body = "";
+
+        res.on("data", (chunk) => {
+            body += chunk;
+        });
+
+        res.on("end", () => {
+            try {
+                let json = JSON.parse(body);
+                console.log(json)//we can manupulate the incoming json here
+            }
+            catch (error) {
+                console.error(error.message);
+            };
+        })
+
+    }).on('error', (e) => {
+        console.error(e);
     });
 
-    res.on("end", () => {
-        try {
-            let json = JSON.parse(body);
-            console.log(json)
+}
 
+
+/**
+ * These function will update the existing content in the web or create a new one based on the url
+ * @param {string} url 
+ * @param {string} updated_data -updated data to be posted in the web
+ */
+function updatedata(url, updated_data) {
+    var options = {
+        hostname: url,
+        method: 'POST',
+        headers: {
+            //information for headers
+            'Content-Length': updated_data.length
         }
-        catch (error) {
-            console.error(error.message);
-        };
-    })
+    };
 
-}).on('error', (e) => {
-  console.error(e);
+    var req = https.request(options, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+
+        res.on('data', (d) => {
+            process.stdout.write(d); //console the responce from the web
+        });
+
+    });
+
+    req.on('error', (e) => {
+        console.error(e);
+    });
+
+    req.write(updated_data); //post the data to the web
+    req.end();
+}
+
+
+var updated_data = querystring.stringify({  //sample data
+    'msg': 'Hello World!'
 });
-
-
-
-/*
-var xml =  '<?xml version="1.0" encoding="UTF-8"?>' +
-           '<root>' +
-               '<child foo="bar">' +
-                   '<grandchild baz="fizbuzz">grandchild content</grandchild>' +
-               '</child>' +
-               '<sibling>with content!</sibling>' +
-           '</root>';
- 
-var xmlDoc = libxmljs.parseXml(xml);
- 
-// xpath queries
-var gchild = xmlDoc.get('//grandchild');
- 
-console.log(gchild.text());  // prints "grandchild content"
- 
-var children = xmlDoc.root().childNodes();
-var child = children[0];
- 
-console.log(child.attr('foo').value());
-*/
